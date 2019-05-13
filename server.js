d9a883902eeb00d6a801972ddc3925edaef91082
Michael <mball.28@outlook.com>
2019-05-13 11:20:06 -0600

    heroku data

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',//this is the address for home
+    host : 'evening-reaches-71208',//this is the address for home
     user : 'postgres',
     password : 'mypostgresql',
     database : 'smart-brain'
