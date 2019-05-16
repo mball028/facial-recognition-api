4a8d77a234530381113e3e271fa79fc50be05c3e
Michael <mball.28@outlook.com>
2019-05-16 11:56:44 -0600

    params id

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
-app.delete('/deleteuser', (req, res) => {
-  const { id } = req.body;
-    db.from('login').where({id: id})
+app.delete('/deleteuser/:id', (req, res) => {
+  const { id } = req.params;
+    db.from('login').where('id', '=', id)
     .del()
     .then(() => {
       db.select()
